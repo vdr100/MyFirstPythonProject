e534ad8f936e716ec813593434353a5b11515212
null null <labtestqc100@gmail.com>
2020-05-25 01:10:00 +0000

    age

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-x = 5 
-x /= 2
+x=int(input ("Enter your age"))
 
-print (x)
-y = 5
-y += 4
-print ( y)
+if x<4 :
+ print ("you are a toddler")
+elif x<13:
+   print ("child")
+elif x <17 :
+   print ("Teenager")
+else:
+ print ("Adult")
+print ("done")
 
-x=200
-y=200
 
-if (x>y) :
- print (f'x-y= {x-y}')
-elif y>x :
- print (f'y-x= {y-x}')
-else :
-   print (f'x and y are equal')
- 
