87edd78a9a8ac7bfcc0e20eae5f1109a097a1aa8
null null <labtestqc100@gmail.com>
2020-05-25 00:54:13 +0000

    comparison

--- main.py
+++ main.py
@@ -1,51 +1,18 @@
-print("My name is Diya")
-print (10+10)
-print ("My age is " + str(4+5))
-print ("My age using multiplication  is " + str(3*3))
-print (" I learn from " + "repl.it")
-print ( " I am awesome")
-print ( "10*14= " +str (10*14))
-print ( " I am in 4rth grade")
-print ( " Today is Mother's Day ")
-print (" I am going to make a cake")
-print (" I am absolutely awsome!")
-print ( " I learn" +" with Python ")
-print ( " Father's day is in June or July")
-#first_name = input('Enter your First Name : ')
-#last_name = input('Enter your Last Name : ')
-#print ('Your Full Name is ' + first_name + ' ' + last_name)
-x='ad'
-y="strings"
-z='''
-asdf
-asdf
-'''
-a= 10 # integer
-b = 7.9 #decimal float
-light_bulb_on=False #boolean
-weekend=True
-print (type(x))
-print (type(y))
-print (type(z))
-print (type(a))
-print (type(b))
-print (type(light_bulb_on))
-print (light_bulb_on)
-print (int(int(a)+float(b)))
-a = input ('Enter your first number :')
-b = input ( 'Enter you last number  : ')
-a= int(a)
-b= int(b)
-print (f"the sume of {a} + {b}={a+b}")
+x = 5 
+x /= 2
 
-first_name = input('Enter your First Name : ')
-last_name = input('Enter your Last Name : ')
-adress = input('Enter your street adress : ')
-zipcode = input(' Enter your zipcode : ')
-birthyear = int(input ('Enter your birthyear : '))
-age = 2020 - birthyear
-#print (2020- 1098 = (2020) - (1098) = (2020- 1098))
+print (x)
+y = 5
+y += 4
+print ( y)
 
-str1 = 'Hello\' World'
-print(str1)
-print (str1.count('\'')
+x=200
+y=200
+
+if (x>y) :
+ print (f'x-y= {x-y}')
+elif y>x :
+ print (f'y-x= {y-x}')
+else :
+   print (f'x and y are equal')
+ 
